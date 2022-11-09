7d774dbbc3eaae35835637d7a718b09c9be3c45b
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-09 19:25:29 -0400

    Se crea la ruta api/users, que se use en index, y que en post use el método create del controlador

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+let router = require('express').Router();
+let { create } = require('../controllers/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', create);
 
 module.exports = router;
