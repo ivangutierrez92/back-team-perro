308eb09db131df98e706322f06a330ae7af298b2
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-10 12:23:31 -0300

    Agregada ruta para create de cities

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 let router = require('express').Router();
 
 let users = require("./users");
+let cities = require("./cities");
 
 router.use('/api/users', users);
+router.use('/api/cities', cities);
 
 module.exports = router;
