62a8fb1e48aa8d36356759d62fdba489f0c2a5fe
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-11 19:34:23 -0300

    Agregada ruta de itineraries y la ruta de get

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ let router = require("express").Router();
 let users = require("./users");
 let cities = require("./cities");
 let hotels = require("./hotels");
+let itineraries = require("./itineraries");
 
 router.use("/api/users", users);
 router.use("/api/cities", cities);
 router.use("/api/hotels", hotels);
+router.use("/api/itineraries", itineraries);
 
 module.exports = router;
