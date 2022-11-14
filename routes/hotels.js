d055d30dd82b11d8f6e8fdba7b82ee24583ff6d3
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-14 13:04:04 -0300

    MT-B2-30

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
-const { create,read,readOne,update } = require("../controllers/hotel");
+const { create,read,readOne,update,deleteHotel } = require("../controllers/hotel");
 
 router.post("/", create);
 router.get("/", read);
 router.get("/:id", readOne);
 router.patch("/:id", update);
+router.delete("/:id", deleteHotel);
 
 module.exports = router;
