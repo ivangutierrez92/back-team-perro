b81f82a3c331401ff23eee8aa4831fe45fa7fff4
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-21 21:17:41 -0300

    MT-02-42

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 const schema =require('../schemas/hotel')
 const validator= require('../middleware/validatorHotel')
-const { create,read,readOne,update,deleteHotel } = require("../controllers/hotel");
+const {
+  create,
+  read,
+  readOne,
+  update,
+  deleteHotel,
+} = require("../controllers/hotel");
 
 router.post("/",validator(schema),create);
 router.get("/", read);
 router.get("/:id", readOne);
 router.patch("/:id", update);
 router.delete("/:id", deleteHotel);
+
 
 module.exports = router;
