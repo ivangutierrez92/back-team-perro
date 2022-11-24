6508e2e8a521eeffb2ee91d43e9c6e123709b8cd
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-24 17:00:07 -0300

    Agregada ruta de salir

--- routes/auths.js
+++ routes/auths.js
@@ -1,5 +1,5 @@
 let router = require("express").Router();
-let { register, verify } = require("../controllers/auth");
+let { register, verify, exit } = require("../controllers/auth");
 const { accountExists: accountExistsSignUp } = require("../middleware/accountExistsSignUp");
 let validator = require("../middleware/validator");
 let schema = require("../schemas/signup");
@@ -7,4 +7,5 @@ let schema = require("../schemas/signup");
 router.post("/sign-up", validator(schema), accountExistsSignUp, register);
 router.get("/verify/:code", verify);
 
+router.post("/sign-out", exit);
 module.exports = router;
