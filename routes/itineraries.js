f1ed16efbc84b977fa729d5b035d298c98fb0abd
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-26 16:28:55 -0300

    Agregado validador de create

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -3,8 +3,10 @@ const passport = require("../config/passport");
 let { create, read, update, destroy, show } = require("../controllers/itinerary");
 const userIsOwner = require("../middleware/userIsOwner");
 const Itinerary = require("../models/Itinerary");
+const validator = require("../middleware/validator");
+const ItinerarySchema = require("../schemas/itinerary");
 
-router.post("/", passport.authenticate("jwt", { session: false }), create);
+router.post("/", passport.authenticate("jwt", { session: false }), validator(ItinerarySchema), create);
 router.get("/", read);
 router.put("/:id", passport.authenticate("jwt", { session: false }), userIsOwner(Itinerary), update);
 router.delete("/:id", passport.authenticate("jwt", { session: false }), userIsOwner(Itinerary), destroy);
---
+++ schemas/itinerary.js
@@ -0,0 +1,36 @@
+const joi = require("joi");
+
+const ItinerarySchema = joi.object({
+  name: joi.string().required().messages({
+    "any.required": "The field 'name' is required, please enter it",
+    "string.empty": "The field 'name' mustn't be empty, please fill it",
+    "string.base": "The field 'name' must be a string",
+  }),
+  photo: joi.array().items(joi.string().uri()).length(3).required().messages({
+    "any.required": "The field 'photo' is required, please enter it",
+    "string.empty": "The field 'photo' mustn't be empty, please fill it",
+    "array.base": "The field 'photo' must be an Array",
+    "array.length": "The field 'photo' must have three elements",
+    "string.uri": "The content of field 'photo' must be an url",
+  }),
+  price: joi.number().min(0).required().messages({
+    "any.required": "The field 'price' is required, please enter it",
+    "number.base": "The field 'price' must be a number",
+    "number.min": "The field 'price' must be a number greater or equal to 0, please change it",
+  }),
+  duration: joi.number().min(0).required().messages({
+    "any.required": "The field 'duration' is required, please enter it",
+    "number.base": "The field 'duration' must be a number",
+    "number.min": "The field 'duration' must be a number greater or equal to 0, please change it",
+  }),
+  description: joi.string().required().messages({
+    "any.required": "The field 'description' is required, please enter it",
+    "string.empty": "The field 'description' mustn't be empty, please fill it",
+    "string.base": "The field 'description' must be a string",
+  }),
+  cityId: joi.any().required().messages({
+    "any.required": "The field 'userId' is required, please enter it",
+  }),
+});
+
+module.exports = ItinerarySchema;
