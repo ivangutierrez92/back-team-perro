2396b917e7c69f5a50ffbef90799865231c83be0
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-27 11:54:21 -0300

    MT-02-54

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const schema =require('../schemas/hotel')
 const validator= require('../middleware/validator')
+const passport = require("../config/passport");
+const userIsOwner = require("../middleware/userIsOwner");
+const Hotel = require("../models/Hotel")
 const {
   create,
   read,
@@ -12,8 +15,13 @@ const {
 router.post("/",validator(schema),create);
 router.get("/", read);
 router.get("/:id", readOne);
-router.patch("/:id", update);
-router.delete("/:id", deleteHotel);
+router.patch("/:id",passport.authenticate("jwt", { session: false }),update);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  userIsOwner(Hotel),
+  deleteHotel
+);
 
 
 module.exports = router;
--- test/hotel.test.js
+++ test/hotel.test.js
@@ -3,7 +3,7 @@ const chai = require("chai");
 const assert = chai.assert;
 const request = require("supertest");
 
-// ? agrupa los test cases
+// // ? agrupa los test cases
 describe("GET /api/hotels?name=",function(){
 
 // ? it -test cases
@@ -74,4 +74,34 @@ describe("POST,/api/hotels", function(){
     
   })
 })
+
+describe("PATCH,/api/hotels/", function () {
+
+
+  it("La operacion debe ser exitosa", function (done) {
+      let token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2U4MmZkNjg2NzIzMmI3NWFjYThkMiIsIm5hbWUiOiJpdmFuIiwicGhvdG8iOiJodHRwczovL21lZGlhLWV4cDEubGljZG4uY29tL2Rtcy9pbWFnZS9DNEUwM0FRRTBiSTNRb1A5TE1BL3Byb2ZpbGUtZGlzcGxheXBob3RvLXNocmlua184MDBfODAwLzAvMTU0MTI5Nzg1OTQ1Mz9lPTIxNDc0ODM2NDcmdj1iZXRhJnQ9dE5qV2hZcTlGVm8zSXF2aThpc2p2UUhOUTBZZ1EwUmhId1h2TVpQUnRqbyIsImxvZ2dlZCI6dHJ1ZSwiaWF0IjoxNjY5NTU4NjM1LCJleHAiOjE2Njk2NDUwMzV9.s7M_xOFShZm9Gftpxhulur3q0fInsS3zuHlVfGafuKw";
+
+
+
+     request(app)
+      .patch("/api/hotels/636d2ce27c9781cb6b0bc8cb")
+      .send({
+        "name": "Hotel Testeado",
+      })
+      .auth(token,{type:"bearer"})
+      .expect((response) => {
+        assert.equal(response.status, 200);
+      })
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
+
+
+
 
