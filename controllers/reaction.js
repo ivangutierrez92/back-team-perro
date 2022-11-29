ea94a2dc14a58f7c0b9403e79462877ef35f5db0
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-29 18:10:38 -0300

    Agregada ruta para agregar o quitar

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -4,7 +4,7 @@ const { errorMessage } = require("../utils/utils");
 
 const controller = {
   create: async (req, res) => {
-    let {body} = req;
+    let { body } = req;
     body.userId = [];
     try {
       let itinerary = await Itinerary.findOne({ _id: body.itineraryId });
@@ -20,6 +20,37 @@ const controller = {
       }
     } catch (error) {
       errorMessage(res, 400, error.message);
+    }
+  },
+  update: async (req, res) => {
+    let { query, user } = req;
+    if (query.name && query.itineraryId) {
+      let { name, itineraryId } = query;
+      let reaction = await Reaction.findOne({ name, itineraryId });
+
+      if (reaction) {
+        if (reaction.userId.some(reactionUser => reactionUser.equals(user.id))) {
+          reaction.userId.pull(user.id);
+          reaction.save();
+          res.status(200).json({
+            success: true,
+            response: { id: reaction._id, name: reaction.name, length: reaction.userId.length },
+            message: "The user unreacted to the reaction",
+          });
+        } else {
+          reaction.userId.push(user.id);
+          reaction.save();
+          res.status(200).json({
+            success: true,
+            response: { id: reaction._id, name: reaction.name, length: reaction.userId.length },
+            message: "The user reacted to the reaction",
+          });
+        }
+      } else {
+        errorMessage(res, 404, "Couldn't find the reaction");
+      }
+    } else {
+      errorMessage(res, 400, "You need to specify the name and itineraryId");
     }
   },
 };
--- routes/reactions.js
+++ routes/reactions.js
@@ -1,9 +1,10 @@
 let router = require("express").Router();
 const passport = require("../config/passport");
-let { create } = require("../controllers/reaction");
+let { create, update } = require("../controllers/reaction");
 const userIsOwner = require("../middleware/userIsOwner");
 const validator = require("../middleware/validator");
 const reactionSchema = require("../schemas/reaction")
 
 router.post("/", passport.authenticate("jwt", { session: false }), validator(reactionSchema), create);
+router.put("/", passport.authenticate("jwt", { session: false }), update)
 module.exports = router;
