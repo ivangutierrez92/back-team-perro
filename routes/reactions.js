5b773b208c4a61e2a7bf9e5cf390bc49969f7122
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-30 17:06:30 -0300

    Agregado para ver reacciones por usuarios y eliminar reacciones

--- routes/reactions.js
+++ routes/reactions.js
@@ -1,11 +1,13 @@
 let router = require("express").Router();
 const passport = require("../config/passport");
-let { create, read, update } = require("../controllers/reaction");
+let { create, read, update, pullUser } = require("../controllers/reaction");
 const userIsOwner = require("../middleware/userIsOwner");
 const validator = require("../middleware/validator");
+const Reaction = require("../models/Reaction");
 const reactionSchema = require("../schemas/reaction");
 
 router.get("/", passport.authenticate("jwt", { session: false }), read);
 router.post("/", passport.authenticate("jwt", { session: false }), validator(reactionSchema), create);
 router.put("/", passport.authenticate("jwt", { session: false }), update);
+router.put("/:id", passport.authenticate("jwt", { session: false }), userIsOwner(Reaction), pullUser);
 module.exports = router;
