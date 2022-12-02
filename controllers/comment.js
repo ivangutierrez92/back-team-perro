8efe40e41af05ec04a9103ef14261749f6860f18
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-12-02 01:46:54 -0300

    MT-02-58

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,67 +1,112 @@
-const Comment = require('../models/Comment')
+const Comment = require("../models/Comment");
 
-const controller={
+const controller = {
+  read: async (req, res) => {
+    let { showId } = req.query;
+    console.log(req.user);
 
-  read: async (req,res)=>{
-      let {showId}= req.query
-      console.log(req.user)
-
-      try {
-
-      let comment =await Comment.find({showId}).sort({updatedAt:-1}).populate({path:"userId" , select:"name"})
-      if (comment){
+    try {
+      let comment = await Comment.find({ showId })
+        .sort({ updatedAt: -1 })
+        .populate({ path: "userId", select: "name" });
+      if (comment) {
         res.status(200).json({
-          response:comment,
+          response: comment,
           success: true,
-          messsage: "comments found successfully"
-
-        })
-      }else{
-          res.status(404).json({
-            success: false,
-            message: "comments not found"
-          })
-        }
-
-      } catch (error) {
-
+          messsage: "comments found successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "comments not found",
+        });
+      }
+    } catch (error) {
       res.status(400).json({
         success: false,
-        message: error.message
-      })
+        message: error.message,
+      });
+    }
+  },
+
+  create: async (req, res) => {
+    let { user, body } = req;
+    console.log(req.user.id);
+    try {
+      let new_comment = await Comment.create({
+        showId: body.showId,
+        comment: body.comment,
+        userId: user.id,
+        photo: user.photo,
+      });
+      console.log(new_comment);
+      res.status(201).json({
+        response: new_comment,
+        success: true,
+        messsage: "comment created successfully",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  update: async (req, res) => {
+    let { id } = req.params;
+    let update = req.body;
+
+    try {
+      let comment = await Comment.findOneAndUpdate({ _id: id }, update, {
+        new: true,
+      });
+
+      if (comment) {
+        res.status(200).json({
+          id: comment._id,
+          success: true,
+          messsage: "comment updated successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "comment not found",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  destroy: async (req, res) => {
+    let { id } = req.params;
+    try {
+      let comment = await Comment.findOneAndDelete({ _id: id });
+      if (comment) {
+        res.status(200).json({
+          id: comment._id,
+          success: true,
+          messsage: "comment deleted successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "comment not found",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
   },
 
 
-  create: async(req,res)=>{
-
-    let {user,body}=req;
-   console.log(req.user.id)
-    try{
-
-      let new_comment =await Comment.create({showId:body.showId,comment:body.comment,userId:user.id,photo:user.photo});
-      console.log(new_comment)
-      res.status(201).json({
-        response:new_comment,
-        success: true,
-        messsage: "comment created successfully"
-      })
-    }catch(error){
-       res.status(400).json({
-        success: false,
-        message: error.message,
-    })
 
 
-  }
-}
-
-
-
-
-
-}
+};
 
 module.exports = controller;
-
-
--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,15 @@
 let router = require("express").Router();
 const passport = require("../config/passport");
 const validator = require("../middleware/validator");
+const userIsOwner = require("../middleware/userIsOwner");
 const schema = require("../schemas/comment");
-const {read,create}=require("../controllers/comment")
-
+const {read,create,update,destroy}=require("../controllers/comment")
+const Comment= require("../models/Comment")
 router.get("/",read)
 router.post("/", passport.authenticate("jwt", { session: false }),validator(schema),create)
+router.put("/:id",passport.authenticate("jwt", { session: false }),validator(schema),userIsOwner(Comment),update);
+router.delete("/:id",passport.authenticate("jwt",{session:false}),userIsOwner(Comment),destroy);
+
+
 
 module.exports = router;
